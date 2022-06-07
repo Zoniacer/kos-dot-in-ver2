ec21938ce9f9b02e81c6c3e89fabef537de7e2f3
Zoniacer <63639703+Zoniacer@users.noreply.github.com>
2022-06-07 22:54:19 +0700

    added sewa_kos containers for images

--- resources/views/sewa_kos.blade.php
+++ resources/views/sewa_kos.blade.php
@@ -2,3 +2,21 @@
 @section('title')
 Sewa Kos
 @endsection
+@section('isi')
+    <div class="container-md mt-5">
+        <h2>Kost Apik Al Aminy Tipe E Pondokgede Bekasi</h2>
+    </div>
+    <div class="container-md mb-5">
+        <div class="col">
+            <img src="assets/fotokosan/1.jpeg"class="img-fluid rounded float-left m-2" alt="" width="600" height="600">
+        </div>
+        <div class="col">
+            <div class="col 1">
+                <img src="assets/fotokosan/1.jpeg"class="img-fluid rounded float-left m-1" alt="" width="300" height="300">
+            </div>
+            <div class="col 1">
+                <img src="assets/fotokosan/1.jpeg"class="img-fluid rounded float-left m-1" alt="" width="300" height="300">
+            </div>
+        </div>
+    </div>
+@endsection
